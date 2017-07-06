7fcfcff7f887e0b6a0937381db73e709001d8e86
nanafuji <nana.fujiyama@orange.fr>
2017-07-06 19:10:07 +0200

    Exercise 2: Google search bar to show result in Google website

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// Exercise 1
 var afficherBox = document.getElementById('principal');
 
 afficherBox.onclick = function() {
@@ -5,50 +6,18 @@ afficherBox.onclick = function() {
   afficherBox.className="hide";
 }
 
+//Exercise 2
 
-/*
-function afficherBox() {
+var look = document.forms["googleform"].text_search;
+var click = document.forms["googleform"].click_button;
 
+click.onclick = function () {
+   var value = look.value.trim();
+   if (value != "" ) {
+       var objectToSearch = value.replace(/( )+/g, "+");
+       document.location.href="https://www.google.fr/#q=" + objectToSearch;
+   }
+   else {
+   }
 }
 
-elements.onclick = function(){
-  afficherBox();
-}
-
-/*
-
-var monImage = document.querySelector('img');
-
-monImage.onclick = function() {
-    var maSrc = monImage.getAttribute('src');
-    if(maSrc === 'images/firefox-icon.png') {
-      monImage.setAttribute ('src','images/js2.jpg');
-    } else {
-      monImage.setAttribute ('src','images/firefox-icon.png');
-    }
-}
-
-var monBouton = document.querySelector('button');
-var monTitre = document.querySelector('h1');
-
-
-/*
-function définirNomUtilisateur() {
-  var monNom = prompt('Veuillez saisir votre nom.');
-  localStorage.setItem('nom', monNom);
-  monTitre.textContent = 'Mozilla est cool, ' + monNom;
-}
-
-
-if(!localStorage.getItem('nom')) {
-  définirNomUtilisateur();
-} else {
-  var nomEnregistré = localStorage.getItem('nom');
-  monTitre.textContent = 'Mozilla est cool, ' + nomEnregistré;
-}
-
-monBouton.onclick = function() {
-  définirNomUtilisateur();
-}
-
-*/
